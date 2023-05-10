4da4a63603285ac2d0aa5ad567cdc1c61bca6405
Iryna Temchuk <itemchuk98@gmail.com>
2023-05-10 14:30:51 +0300

    Change li

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,83 +1,27 @@
 import React from "react";
 import "./Header.css";
 import Logo from "../../content/logo.png";
-import Bars from "../../content/bars.png";
+import Cart from "../../content/cart.png";
+// import Bars from "../../content/bars.png";
+
 
 const Header = () => {
-    const mobile = window.innerWidth <= 768 ? true : false;
-    const [menuOpened, setMenuOpened] = useState(false);
+
   
     return (
-    <div>
-      <div className="logo">
+    <div className="header">
+
         <img src={Logo} alt="" className="logo" />
-      </div>
-      <div className="menu">
-        {menuOpened === false && mobile === true ? (
-          <div
-            style={{
-              backgroundColor: "var(--appColor)",
-              padding: "0.5rem",
-              borderRadius: "5px",
-            }}
-            onClick={() => setMenuOpened(true)}
-          >
-            <img
-              src={Bars}
-              alt=""
-              style={{ width: "1.5rem", height: "1.5rem" }}
-            />
-          </div>
-        ) : (
+
           <ul className="shop-menu">
-            <li onClick={() => setMenuOpened(false)}>
-            <Link
-              onClick={() => setMenuOpened(false)}
-              activeClass="active"
-              to=""
-              spy={true}
-              smooth={true}
-            >
-              Women
-            </Link>
-          </li>
-          <li onClick={() => setMenuOpened(false)}>
-            <Link
-              onClick={() => setMenuOpened(false)}
-              activeClass="active"
-              to=""
-              spy={true}
-              smooth={true}
-            >
-              Men
-            </Link>
-          </li>
-          <li onClick={() => setMenuOpened(false)}>
-            <Link
-              onClick={() => setMenuOpened(false)}
-              activeClass="active"
-              to=""
-              spy={true}
-              smooth={true}
-            >
-              Kids
-            </Link>
-          </li>
-          <li onClick={() => setMenuOpened(false)}>
-            <Link
-              onClick={() => setMenuOpened(false)}
-              activeClass="active"
-              to=""
-              spy={true}
-              smooth={true}
-            >
-              Home
-            </Link>
-          </li>
+            <li className="women">Women</li>
+            <li className="men">Men</li>
+            <li className="kids">Kids</li>
+            <li className="home">Home</li>
           </ul>
-        )}
-      </div>
-      <div className="shop-cart"></div>
+
+      <img src={Cart} alt="" className="shop-cart" />
+      
     </div>
   );
 
