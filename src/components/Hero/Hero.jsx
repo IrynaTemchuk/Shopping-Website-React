75797c8d29de53a4e6b5020bcfc9fae9c18a26c8
Iryna Temchuk <itemchuk98@gmail.com>
2023-05-10 14:30:38 +0300

    Add hero style

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import '<./>Hero.css';
+
+const Hero = () => {
+
+  
+    return (
+    <div className="hero">
+        <img />
+      
+    </div>
+  );
+
+};
+
+export default Hero;
