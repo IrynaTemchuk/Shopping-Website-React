544e80b90e42aaa585b7a353eecf50689c981ab0
Iryna Temchuk <itemchuk98@gmail.com>
2023-05-10 15:11:55 +0300

    Change red to white

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,30 +1,25 @@
 import React from "react";
 import "./Header.css";
-import Logo from "../../content/logo.png";
+import Logo1 from "../../content/logo(1).png";
 import Cart from "../../content/cart.png";
 // import Bars from "../../content/bars.png";
 
-
 const Header = () => {
-
-  
-    return (
+  return (
     <div className="header">
+      <img src={Logo1} alt="" className="logo" />
 
-        <img src={Logo} alt="" className="logo" />
-
-          <ul className="shop-menu">
-            <li className="women">Women</li>
-            <li className="men">Men</li>
-            <li className="kids">Kids</li>
-            <li className="home">Home</li>
-          </ul>
+      <ul className="shop-menu">
+        <li className="main">Main</li>
+        <li className="women">Women</li>
+        <li className="men">Men</li>
+        <li className="kids">Kids</li>
+        <li className="home">Home</li>
+      </ul>
 
       <img src={Cart} alt="" className="shop-cart" />
-      
     </div>
   );
-
 };
 
 export default Header;
