3f000f286e0557f71a70462b67f0861b8e057270
Iryna Temchuk <itemchuk98@gmail.com>
2023-05-10 16:09:16 +0300

    Delete routes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,13 @@
 import './App.css';
 import Header from './components/Header/Header';
 import Hero from './components/Hero/Hero';
-import Navbar from './components/Navbar';
-import { BrowserRouter as Router, Routes, Route}
-    from 'react-router-dom';
-import Main from './pages/main';
-import Women from './pages/women';
-import Men from './pages/men';
-import Kids from './pages/kids';
-import Home from './pages/home';
+
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Hero />
-      <Router>
-    <Navbar />
-    <Routes>
-        <Route path='/main' element={<Main/>} />
-        <Route path='/women' element={<Women/>} />
-        <Route path='/men' element={<Men/>} />
-        <Route path='/kids' element={<Kids/>} />
-        <Route path='/home' element={<Home/>} />
-    </Routes>
-    </Router>
     </div>
   );
 }
--- src/components/Pages/men.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-  
-const Men = () => {
-  return (
-    <h1>You can write your blogs!</h1>
-  );
-};
-  
-export default Men;
