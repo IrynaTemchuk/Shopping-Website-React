076839bb20d5631633e836557530429f4fd2e513
Iryna Temchuk <itemchuk98@gmail.com>
2023-05-10 15:12:16 +0300

    Add hero section with style

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
-import '<./>Hero.css';
+import React from "react";
+import "./Hero.css";
+import Home from "../../content/Home.png"
 
 const Hero = () => {
 
   
     return (
     <div className="hero">
-        <img />
-      
+        <img src={Home} alt="" className="cover"/>
+        <h1 className="cover-header">Made for comfort</h1>
     </div>
   );
 
