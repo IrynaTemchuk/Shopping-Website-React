1483af470445c3637b4a58dad2eb4e96b8f7764e
Iryna Temchuk <itemchuk98@gmail.com>
2023-05-10 15:25:14 +0300

    Add cover-text with style

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -9,6 +9,7 @@ const Hero = () => {
     <div className="hero">
         <img src={Home} alt="" className="cover"/>
         <h1 className="cover-header">Made for comfort</h1>
+        <h4 className="cover-text">Explore our breezy summer collection now</h4>
     </div>
   );
 
