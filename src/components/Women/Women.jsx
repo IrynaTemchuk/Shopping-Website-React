bd0b39223ef8ab8085196c78d8741f79c5c3fe7b
Iryna Temchuk <itemchuk98@gmail.com>
2023-05-10 18:26:20 +0300

    Add women chapter with style and content

--- src/components/Women/Women.jsx
+++ src/components/Women/Women.jsx
@@ -1,12 +1,96 @@
 import React from "react";
 import "./Women.css";
+import Blouse4 from "../../content/blouse4.jpg";
+import Blouse2 from "../../content/blouse2.jpg";
 
 const Women = () => {
-
-    return (
-        <div>Hi</div>
-
-    );
+  return (
+    <div className="women-chapter">
+      <div className="carousel">
+        {/* first position */}
+        <div className="women-content">
+          <div className="card">
+            <img src={Blouse4} className="blouse" />
+            <img src={Blouse2} className="blouse-hover" />
+          </div>
+          <div className="description">
+            <h4 className="name">Linen blouse</h4>
+            <p className="blouse-desc">
+              Cropped slot collar blouse with long sleeves. <br />
+              Matching covered knot fastening at the front.
+            </p>
+            <hr />
+            <p className="size-head">Size:</p>
+            <ul>
+              <li className="size">XS</li>
+              <li className="size">S</li>
+              <li className="size">M</li>
+              <li className="size">L</li>
+              <li className="size">XL</li>
+            </ul>
+            <hr />
+            <h4 className="price">$100</h4>
+            <hr />
+            <button className="buy">Buy Now</button>
+          </div>
+        </div>
+        {/* second position */}
+        <div className="women-content">
+          <div className="card">
+            <img src={Blouse4} className="blouse" />
+            <img src={Blouse2} className="blouse-hover" />
+          </div>
+          <div className="description">
+            <h4 className="name">Linen blouse</h4>
+            <p className="blouse-desc">
+              Cropped slot collar blouse with long sleeves. <br />
+              Matching covered knot fastening at the front.
+            </p>
+            <hr />
+            <p className="size-head">Size:</p>
+            <ul>
+              <li className="size">XS</li>
+              <li className="size">S</li>
+              <li className="size">M</li>
+              <li className="size">L</li>
+              <li className="size">XL</li>
+            </ul>
+            <hr />
+            <h4 className="price">$100</h4>
+            <hr />
+            <button className="buy">Buy Now</button>
+          </div>
+        </div>
+        {/* third position */}
+        <div className="women-content">
+          <div className="card">
+            <img src={Blouse4} className="blouse" />
+            <img src={Blouse2} className="blouse-hover" />
+          </div>
+          <div className="description">
+            <h4 className="name">Linen blouse</h4>
+            <p className="blouse-desc">
+              Cropped slot collar blouse with long sleeves. <br />
+              Matching covered knot fastening at the front.
+            </p>
+            <hr />
+            <p className="size-head">Size:</p>
+            <ul>
+              <li className="size">XS</li>
+              <li className="size">S</li>
+              <li className="size">M</li>
+              <li className="size">L</li>
+              <li className="size">XL</li>
+            </ul>
+            <hr />
+            <h4 className="price">$100</h4>
+            <hr />
+            <button className="buy">Buy Now</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Women;
