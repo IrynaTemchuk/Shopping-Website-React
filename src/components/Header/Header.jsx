ed47654bcaef6dda19fac4791cfa4166b83156af
Iryna Temchuk <itemchuk98@gmail.com>
2023-05-10 12:59:56 +0300

    Add header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,86 @@
-import React from 'react';
+import React from "react";
 import "./Header.css";
+import Logo from "../../content/logo.png";
+import Bars from "../../content/bars.png";
 
-export default function Header() {
-  return (
-    <div>Header</div>
-  )
-}
+const Header = () => {
+    const mobile = window.innerWidth <= 768 ? true : false;
+    const [menuOpened, setMenuOpened] = useState(false);
+  
+    return (
+    <div>
+      <div className="logo">
+        <img src={Logo} alt="" className="logo" />
+      </div>
+      <div className="menu">
+        {menuOpened === false && mobile === true ? (
+          <div
+            style={{
+              backgroundColor: "var(--appColor)",
+              padding: "0.5rem",
+              borderRadius: "5px",
+            }}
+            onClick={() => setMenuOpened(true)}
+          >
+            <img
+              src={Bars}
+              alt=""
+              style={{ width: "1.5rem", height: "1.5rem" }}
+            />
+          </div>
+        ) : (
+          <ul className="shop-menu">
+            <li onClick={() => setMenuOpened(false)}>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              activeClass="active"
+              to=""
+              spy={true}
+              smooth={true}
+            >
+              Women
+            </Link>
+          </li>
+          <li onClick={() => setMenuOpened(false)}>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              activeClass="active"
+              to=""
+              spy={true}
+              smooth={true}
+            >
+              Men
+            </Link>
+          </li>
+          <li onClick={() => setMenuOpened(false)}>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              activeClass="active"
+              to=""
+              spy={true}
+              smooth={true}
+            >
+              Kids
+            </Link>
+          </li>
+          <li onClick={() => setMenuOpened(false)}>
+            <Link
+              onClick={() => setMenuOpened(false)}
+              activeClass="active"
+              to=""
+              spy={true}
+              smooth={true}
+            >
+              Home
+            </Link>
+          </li>
+          </ul>
+        )}
+      </div>
+      <div className="shop-cart"></div>
+    </div>
+  );
+
+};
+
+export default Header;
